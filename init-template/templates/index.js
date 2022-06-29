c3c6aa1c2a25b2935e3b191d418c843e82d692ec
Rosiney Gomes Pereira <rosiney.pereira@stackspot.com>
2022-06-29 13:50:32 -0300

    feat: increase initial application complexity

--- init-template/templates/index.js
+++ init-template/templates/index.js
@@ -1,1 +1,18 @@
-console.log('hello template')
+const pomParser = require("pom-parser");
+
+const parse = function (file, fn) {
+    pomParser.parse({ filePath: file }, (err, pomResponse) => {
+        if (err) {
+            console.log("ERROR: " + err);
+            process.exit(1);
+        }
+
+        fn(pomResponse)
+    })
+}
+
+x = parse('data/pom.xml', r => {
+    // console.log(r)
+    console.log(JSON.stringify(r.pomObject, null, 4));
+})
+
